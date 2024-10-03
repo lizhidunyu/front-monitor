c8a6a53e9119c02fc14945d8e0bba2d28d783d5a
lzdy <1743995376@qq.com>
2024-10-03 22:29:47 +0800

    fix: 移除无用文件

--- packages/core/dist/src/error.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-Object.defineProperty(exports, '__esModule', { value: true })
-exports.error = error
-function error() {
-  // 监听js错误
-  window.onerror = (msg, url, line, column, error) => {
-    console.log(msg, url, line, column, error)
-  }
-}
--- packages/performance/dist/performance/constants/index.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-Object.defineProperty(exports, '__esModule', { value: true })
-exports.TTFB_RANGE = void 0
-exports.TTFB_RANGE = {
-  PRETTY: 200,
-  ACCEPTED: 500
-}
